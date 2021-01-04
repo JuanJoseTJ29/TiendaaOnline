67b0beb8788e5a236d0d7957a12fd530c4c7a669
Alexis RM <alexis.rojas1@unmsm.edu.pe>
2021-01-04 02:31:46 -0500

    Añadiendo Interfaz nueva a la pagina principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,71 @@
 @extends('adminlte::page')
 
+@section('title', 'Service Home Store')
+@section('content_header')
+    <h1 class="font-italic font-weight-bold p-3 mb-2 bg-info rounded">Servicios Disponibles</h1>
+    @if($busqueda ?? '')
+        <h5 class="m-0 text-dark">Buscando: "{{$busqueda ?? ''}}"</h5>
+    @endif
+@stop
+
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Service Home Store') }}</div>
+<div class="mw-100 ">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded",()=>{
+            const rows = document.querySelectorAll("tr[data-href]");
+            
+            rows.forEach(row=>{
+                row.addEventListener("click",()=>{
+                    window.location.href = row.dataset.href;  
+                });
+            });
+        });
+    </script>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <style>
+        tr[data-href]{
+            cursor:pointer;
+        }
+    </style>
 
-                    {{ __('Has iniciado sesión satisfactoriamente!') }}
-                </div>
-            </div>
+    <table class="table table-striped table-hover table-rounded ">
+        <thead class="thead-dark bg-info">
+            <tr>
+                <th style="text-align: center"><i class="fas fa-camera">  </i></th>
+                <th style="text-align: center"><i class="fas fa-tools"></i> Servicio Ofrecido</th>
+                <th style="text-align: center"><i class="fas fa-hand-holding-usd"></i> Precio(S/. soles) </th>
+                <th style="text-align: center"><i class="fas fa-id-card"></i> Técnico al mando</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach($services as $service)
+            <tr data-href="{{ route('service.show', $service) }}">
+            <td style="text-align: center" >
+                
+                    <img src="{{asset('storage').'/'.$service->picture_path}}" class="shadow-lg rounded" alt="..." width="200">
+                
+                </td>
+                <td style="text-align: center">{{$service->title}}</td>
+                <td> <a href="{{ route('profile.show', $service->profile) }}">{{$service->profile->firstname . " " . $service->profile->lastname}}</a></td>
+                <td style="text-align: center">S/.{{$service->price}}</td>
+                </tr>
+            
+            @endforeach
+        </tbody>
+        
+    </table>
+    <div class="row">
+        <div class="col">
+            {{$services->links('layouts.pie')}}    
+        </div>
+        <div class="col">
+            {{$services->links('layouts.cabecera')}}
         </div>
     </div>
+
 </div>
-@endsection
+
+@stop
---
+++ resources/views/layouts/pie.blade.php
@@ -0,0 +1,24 @@
+@if ($paginator->hasPages())
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">        
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <li class="page-item disabled "><a class="page-link">{{ $element }}</a></li>
+                @endif
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="page-item active" aria-current="page"><span class="page-link">Pág. {{ $page }}</span></li>
+                        @else
+                            <li class="page-item "><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+        </ul>
+    </nav>
+@endif
